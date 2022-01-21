d97fc00c91d0b7a5a33fbda63e60a00bb7097a97
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2022-01-21 09:32:00 +0700

    Update Md_kritik.php
    little modify

--- application/models/Md_kritik.php
+++ application/models/Md_kritik.php
@@ -6,13 +6,6 @@ class Md_kritik extends CI_Model {
 
 	
     var $table = 'db_kritik';
-	/*     * * BEGIN COMPONENT DATA TABLE ** */
-
-	// function getRuanganAll() {
-	// 	return $this->db->get_where($this->table, array('status' => 1, ))->result();
-	// }
-
-    //test
 
     public function tampilan()
     {
@@ -22,46 +15,5 @@ class Md_kritik extends CI_Model {
     public function addEvent($data) {
         $this->db->insert($this->table, $data);
     }
-
-    // public function updateEvent($id, $data) {
-    //     $this->db->where('id', $id)
-    //             ->update($this->table, $data);
-    // }
-
-
-    // function Acara() {
-    //     return $this->db->get_where($this->table)->result();
-    // }
-
-    // function getacaraAll() {
-    //     return $this->db->get_where($this->table, array('tanggal' => $date, ))->result();
-    // }
-
-    // function test() {
-    //     $tanggal = date('Y-m-d');
-    //     $this->db->where('tanggal', $tanggal);
-    //     return $this->db->get()->result();
-    // }
-
-    // function getRuanganaja() {
-    //     $tanggal = date('Y-m-d');
-    //     return $this->db->get_where($this->table, array('tanggal' => $tanggal, 'status' => 1))->result();
-    // }
-
-    // public function deleteEvent($id)
-    // {
-    //      $hasil = $this->db->query("DELETE FROM acara WHERE id='$id'");
-    //     return $hasil;
-    // }
-
-    //end test
-
-
-    
-
-   
-
-   
-
     
 }
