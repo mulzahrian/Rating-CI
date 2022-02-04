bbb9dd0212c3628b5b97a89ab6b329a92fde593b
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2022-02-04 14:01:52 +0700

    Update test.php
    little modify

--- application/views/admin/test.php
+++ application/views/admin/test.php
@@ -4,27 +4,12 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>AdminLTE 2 | Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="<?php echo base_url();?>bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url();?>bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
   <link rel="stylesheet" href="<?php echo base_url();?>bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url();?>dist/css/AdminLTE.min.css">
-  <!-- iCheck -->
   <link rel="stylesheet" href="<?php echo base_url();?>plugins/iCheck/square/blue.css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-
-<!-- Google Font -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -34,7 +19,6 @@
 <style>
 .checked {
     color: orange;
-    
 }
 </style>
 </head>
@@ -42,13 +26,9 @@
   <div class="login-box">
     <div class="login-logo">
       <a><b>Masukan Dan Saran</b></a>
-      
     </div>
-    <!-- /.login-logo -->
     <div class="login-box-body">
       <p class="login-box-msg"></p>
-
-     
         <?php if ($this->session->flashdata('pesan')) { ?>
           <div class="alert alert-info alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
@@ -68,7 +48,6 @@
           <?php echo $this->session->flashdata('gagal'); ?>
         </div>
       <?php } ?>
-
       <div class="col-sm-12 ">
       <?php echo form_open('admin/add_kritik', array('class' => 'form-horizontal')) ?>
                         <div class="contact-form">
@@ -84,19 +63,14 @@
                                 <div class="form-group">
                                     <textarea name="masukkan" class="form-control" rows="8" placeholder="Message"></textarea>
                                 </div>
-                                 <div class="form-group">
-                                    
+                                 <div class="form-group">                      
 <span  onmouseover="starmark(this)" onclick="starmark(this)" id="1one" style="font-size:40px;cursor:pointer;"  class="fa fa-star checked"></span>
 <span onmouseover="starmark(this)" onclick="starmark(this)" id="2one"  style="font-size:40px;cursor:pointer;" class="fa fa-star "></span>
 <span onmouseover="starmark(this)" onclick="starmark(this)" id="3one"  style="font-size:40px;cursor:pointer;" class="fa fa-star "></span>
 <span onmouseover="starmark(this)" onclick="starmark(this)" id="4one"  style="font-size:40px;cursor:pointer;" class="fa fa-star"></span>
 <span onmouseover="starmark(this)" onclick="starmark(this)" id="5one"  style="font-size:40px;cursor:pointer;" class="fa fa-star"></span>
                                 </div>
-                <!-- test -->
-
                 <input type="hidden"  name="bintang" onkeyup="result()" class="form-control" placeholder="Subject">
-
-                <!-- end -->
                         </div>
                     </div>
       <div class="row">
@@ -105,28 +79,18 @@
             <label></label>
           </div>
         </div>
-        <!-- /.col -->
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
         </div>
-        <!-- /.col -->
       </div>
     </form>
   </div>
-  <!-- /.login-box-body -->
 </div>
-<!-- /.login-box -->
-
-<!-- jQuery 3 -->
 <script src="<?php echo base_url();?>bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
 <script src="<?php echo base_url();?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- iCheck -->
 <script src="<?php echo base_url();?>plugins/iCheck/icheck.min.js"></script>
-
 <script>
 var count;
-
 function starmark(item)
 {
 count=item.id[0];
@@ -142,12 +106,8 @@ else
 {
 document.getElementById((i+1)+subid).style.color="black";
 }
-
-
 }
-
 }
-
 function result()
 {
 if (!isNaN(count)) {
@@ -158,8 +118,6 @@ $('input[name="bintang"]').val(0);
 }
 
 }
-
 </script>
-
 </body>
 </html>
