da6dbe2e50a63ec863100af9448ce3b85851a811
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2022-02-08 11:58:57 +0700

    Update coba.php
    little modify

--- application/views/admin/coba.php
+++ application/views/admin/coba.php
@@ -26,15 +26,6 @@
   <link rel="stylesheet" href="<?php echo base_url();?>dist/css/AdminLTE.min.css">
   <!-- iCheck -->
   <link rel="stylesheet" href="<?php echo base_url();?>plugins/iCheck/square/blue.css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-
-<!-- Google Font -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -125,9 +116,6 @@
                           </table>
                         </div>
                     </div>
-
-                     <!-- Modal delete -->
-
             <?php 
               foreach($isi as $list){
                 ?>
@@ -149,34 +137,31 @@
                           <?php echo form_close(); ?>
                         </div>
                       </div>
-                      <!-- /.modal-content -->
+
                     </div>
-                    <!-- /.modal-dialog -->
+
                   </div>
                 <?php }?>
 
-                <!-- tutup modal delete -->
+
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox icheck">
             <label></label>
           </div>
         </div>
-        <!-- /.col -->
-       
-        <!-- /.col -->
+
       </div>
     </form>
   </div>
-  <!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
 
-<!-- jQuery 3 -->
+</div>
+
+
 <script src="<?php echo base_url();?>bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
+
 <script src="<?php echo base_url();?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- iCheck -->
+
 <script src="<?php echo base_url();?>plugins/iCheck/icheck.min.js"></script>
 
 <script>
